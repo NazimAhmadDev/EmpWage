31fd42b1f0caa85f8ac0950502cbbf8d41678cb3
Nazim Ahmad <nazimahmad@Nazims-MacBook-Air.local>
2024-12-18 15:32:19 +0530

    [nazimahmad]update: EmployeeBased_problem1.py file is updated

--- EmployeeBased_problem1.py
+++ EmployeeBased_problem1.py
@@ -2,6 +2,8 @@ import random
 
 print("Welcome to Employee Wage Computation Program on Master Branch")
 
+
+
 # UC-1
 def check_status(name):
     status = random.choice(['Present','Absent'])
@@ -30,14 +32,26 @@ def Add_partTime_wage(name,amount,wage_per_hour,partTime_hour):
     status = check_status(name)
     if(status == 'Present'):
         new_amount = amount + partTime_wage
-        print(f"Your part time amount is also added :",new_amount)
+        print(f"Your part time amount is also added : ${new_amount}")
     else:
         print(f"Your amount is :",amount)
 
 
 
+print("Enter 1 for checking status")
+print("Enter 2 for checking the wage")
+print("Enter 3 for checking the dailyWage + partTimeWage")
+Choice = int(input("Enter your choice :"))
 
-
-# check_status("Nazim")
-# daily_wage("Nazim",20,8)
-Add_partTime_wage("Nazim",160,20,8)
+match Choice:
+    case 1:
+        check_status("Nazim")
+    
+    case 2:
+        daily_wage("Nazim",20,8)
+    
+    case 3:
+        Add_partTime_wage("Nazim",160,20,8)
+    
+    case _:
+        print("Invalid choice!!!")
