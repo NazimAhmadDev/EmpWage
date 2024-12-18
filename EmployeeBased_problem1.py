130c346ae5c3e7352d880906196c6357256c4ae8
Nazim Ahmad <nazimahmad@Nazims-MacBook-Air.local>
2024-12-18 15:10:33 +0530

    [nazimahmad]update: EmployeeBased_problem1.py file is updated

--- EmployeeBased_problem1.py
+++ EmployeeBased_problem1.py
@@ -21,6 +21,23 @@ def daily_wage(name,wage_per_hour,full_day_hour):
         print(f"You don't get your todays wage!!!")
     return amount
 
+# UC-3
+def Add_partTime_wage(name,amount,wage_per_hour,partTime_hour):
+    # Calculating part time wage
+    partTime_wage = wage_per_hour * partTime_hour
+
+    # Checking if employee is present in part time 
+    status = check_status(name)
+    if(status == 'Present'):
+        new_amount = amount + partTime_wage
+        print(f"Your part time amount is also added :",new_amount)
+    else:
+        print(f"Your amount is :",amount)
+
+
+
+
 
 # check_status("Nazim")
-daily_wage("Nazim",20,8)
+# daily_wage("Nazim",20,8)
+Add_partTime_wage("Nazim",160,20,8)
