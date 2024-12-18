0af4515275d6bcdfd279ddd0758441426c40ccd0
Nazim Ahmad <nazimahmad@Nazims-MacBook-Air.local>
2024-12-18 15:07:19 +0530

    [nazimahmad]update: EmployeeBased_problem1.py file is updated

--- EmployeeBased_problem1.py
+++ EmployeeBased_problem1.py
@@ -9,4 +9,18 @@ def check_status(name):
     return status
 
 
-check_status("Nazim")
+# UC-2
+def daily_wage(name,wage_per_hour,full_day_hour):
+    # Checking status of employee is present or absent
+    status = check_status(name)
+
+    if(status == 'Present'):
+        amount = wage_per_hour * full_day_hour
+        print(f"Your wage is ${amount}")
+    else:
+        print(f"You don't get your todays wage!!!")
+    return amount
+
+
+# check_status("Nazim")
+daily_wage("Nazim",20,8)
