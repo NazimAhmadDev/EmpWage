3b163e9d27b111d27c41eeee2fbfdabcee9e4d05
Nazim Ahmad <nazimahmad@Nazims-MacBook-Air.local>
2024-12-20 10:02:19 +0530

    [nazimahmad]update: EmployeeBased_problem1.py file is updated

--- EmployeeBased_problem1.py
+++ EmployeeBased_problem1.py
@@ -19,9 +19,10 @@ def daily_wage(name,wage_per_hour,full_day_hour):
     if(status == 'Present'):
         amount = wage_per_hour * full_day_hour
         print(f"Your wage is ${amount}")
+        return amount
     else:
         print(f"You don't get your todays wage!!!")
-    return amount
+    return 0
 
 # UC-3
 def Add_partTime_wage(name,amount,wage_per_hour,partTime_hour):
@@ -33,14 +34,41 @@ def Add_partTime_wage(name,amount,wage_per_hour,partTime_hour):
     if(status == 'Present'):
         new_amount = amount + partTime_wage
         print(f"Your part time amount is also added : ${new_amount}")
+        return new_amount
     else:
         print(f"Your amount is :",amount)
+    return 0
+
+
+# Uc-5
+def calculate_monthly_wage(name,wage_per_hour,full_day_hour,partTime_hour):
+    monthly_wage = 0
+    working_days = 20
+    partTime_days = 10
+
+    for day in range(1, working_days + 1):
+        print(f"\nDay {day}:")
+        # Check daily status
+        amount = daily_wage(name, wage_per_hour, full_day_hour)
+        
+        # Add part-time wage if he is present
+        if day <= partTime_days:  
+            amount = Add_partTime_wage(name, amount, wage_per_hour, partTime_hour)
+        
+        monthly_wage += amount
+
+    print(f"\n{name}'s total monthly wage is: ${monthly_wage}")
+    return monthly_wage
+        
+
 
 
 
 print("Enter 1 for checking status")
 print("Enter 2 for checking the wage")
 print("Enter 3 for checking the dailyWage + partTimeWage")
+print("Enter 4 for checking the monthlyWage")
+
 Choice = int(input("Enter your choice :"))
 
 match Choice:
@@ -52,6 +80,9 @@ match Choice:
     
     case 3:
         Add_partTime_wage("Nazim",160,20,8)
+
+    case 4:
+        calculate_monthly_wage("Nazim",20,8,4)
     
     case _:
         print("Invalid choice!!!")
