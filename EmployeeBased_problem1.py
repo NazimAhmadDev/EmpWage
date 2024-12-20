95cd65a15051b6ed5d51b1c79d0edf78c65103ed
Nazim Ahmad <nazimahmad@Nazims-MacBook-Air.local>
2024-12-20 10:52:54 +0530

    [nazimahmad]update: UC-7 is completed

--- EmployeeBased_problem1.py
+++ EmployeeBased_problem1.py
@@ -1,79 +1,90 @@
 import random
 
+class EmployeeWageComputation:
+
+    def __init__(self,name,wage_per_hour,full_day_hour,partTime_hour):
+        self.name = name
+        self.wage_per_hour = wage_per_hour
+        self.full_day_hour = full_day_hour
+        self.partTime_hour = partTime_hour
+        self.monthly_hour = 100
+
+    # UC-1
+    def check_status(self):
+        status = random.choice(['Present','Absent'])
+        print(f"{self.name} is {status}")
+        return status
+
+
+    # UC-2
+    def daily_wage(self):
+        # Checking status of employee is present or absent
+        status = self.check_status()
+
+        if(status == 'Present'):
+            amount = self.wage_per_hour * self.full_day_hour
+            print(f"Your wage is ${amount}")
+            return amount
+        else:
+            print(f"You don't get your todays wage!!!")
+            return 0
+
+    # UC-3
+    def Add_partTime_wage(self,amount):
+        # Calculating part time wage
+        partTime_wage = self.wage_per_hour * self.partTime_hour
+
+        # Checking if employee is present in part time 
+        status = self.check_status()
+        if(status == 'Present'):
+            new_amount = amount + partTime_wage
+            print(f"Your part time amount is also added : ${new_amount}")
+            return new_amount
+        else:
+            print(f"Your amount is :",amount)
+            return 0
+
+
+
+    # Uc-5
+    def calculate_monthly_wage(self):
+        monthly_wage = 0
+        working_days = 20
+        part_time_days = 10
+
+        for day in range(1, working_days + 1):
+            print(f"\nDay {day}:")
+            daily_amount = self.daily_wage()
+            if day <= part_time_days:
+                daily_amount = self.Add_partTime_wage(daily_amount)
+            monthly_wage += daily_amount
+        print(f"\n{self.name}'s total monthly wage is: ${monthly_wage}")
+        return monthly_wage
+
+
+    # UC-6
+    def working_hour_reached(self,your_total_working_hour):
+        monthly_hour = 100
+        if(your_total_working_hour <= self.monthly_hour) :
+            your_total_working_hour_wage = self.wage_per_hour * your_total_working_hour
+            print(f"{self.name} your total monthly working hour wage is: ${your_total_working_hour_wage}")
+            return your_total_working_hour_wage
+        else:
+            return 0
+
+        
+
+
+# Main
+
 print("Welcome to Employee Wage Computation Program on Master Branch")
 
+name = input("Enter your name :")
+wage_per_hour = int(input("Enter wage per hour :"))
+full_day_hour = int(input("Enter full day hours :"))
+partTime_hour = int(input("Enter your part time hours :"))
 
-
-# UC-1
-def check_status(name):
-    status = random.choice(['Present','Absent'])
-    print(f"{name} is {status}")
-    return status
-
-
-# UC-2
-def daily_wage(name,wage_per_hour,full_day_hour):
-    # Checking status of employee is present or absent
-    status = check_status(name)
-
-    if(status == 'Present'):
-        amount = wage_per_hour * full_day_hour
-        print(f"Your wage is ${amount}")
-        return amount
-    else:
-        print(f"You don't get your todays wage!!!")
-    return 0
-
-# UC-3
-def Add_partTime_wage(name,amount,wage_per_hour,partTime_hour):
-    # Calculating part time wage
-    partTime_wage = wage_per_hour * partTime_hour
-
-    # Checking if employee is present in part time 
-    status = check_status(name)
-    if(status == 'Present'):
-        new_amount = amount + partTime_wage
-        print(f"Your part time amount is also added : ${new_amount}")
-        return new_amount
-    else:
-        print(f"Your amount is :",amount)
-    return 0
-
-
-# Uc-5
-def calculate_monthly_wage(name,wage_per_hour,full_day_hour,partTime_hour):
-    monthly_wage = 0
-    working_days = 20
-    partTime_days = 10
-
-    for day in range(1, working_days + 1):
-        print(f"\nDay {day}:")
-        # Check daily status
-        amount = daily_wage(name, wage_per_hour, full_day_hour)
-        
-        # Add part-time wage if he is present
-        if day <= partTime_days:  
-            amount = Add_partTime_wage(name, amount, wage_per_hour, partTime_hour)
-        
-        monthly_wage += amount
-
-    print(f"\n{name}'s total monthly wage is: ${monthly_wage}")
-    return monthly_wage
-
-# UC-6
-def working_hour_reached(name,wage_per_hour,your_total_working_hour):
-    monthly_hour = 100
-
-    if(your_total_working_hour <= monthly_hour) :
-        your_total_working_hour_wage = wage_per_hour * your_total_working_hour
-        print(f"{name} your total monthly working hour wage is: ${your_total_working_hour_wage}")
-        return your_total_working_hour_wage
-    else:
-        return 0
-
-        
-
-
+employee = EmployeeWageComputation(name,wage_per_hour,full_day_hour,partTime_hour)
 
 
 print("Enter 1 for checking status")
@@ -86,19 +97,21 @@ Choice = int(input("Enter your choice :"))
 
 match Choice:
     case 1:
-        check_status("Nazim")
+        employee.check_status()
     
     case 2:
-        daily_wage("Nazim",20,8)
+        employee.daily_wage()
     
     case 3:
-        Add_partTime_wage("Nazim",160,20,8)
+        amount = employee.daily_wage()
+        employee.Add_partTime_wage(amount)
 
     case 4:
-        calculate_monthly_wage("Nazim",20,8,4)
+        employee.calculate_monthly_wage()
 
     case 5:
-        working_hour_reached("Nazim",20,100)
+        your_total_working_hour = int(input("Enter your total working hour in a month :"))
+        employee.working_hour_reached(your_total_working_hour)
     
     case _:
         print("Invalid choice!!!")
